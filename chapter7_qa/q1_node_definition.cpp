acfa78d6fa1c013308d462a393721e4a9922bde1
Mohammad Shiekh Ghazanfar <skghazanfar@gmail.com>
2019-08-25 17:33:18 +0600

    Update q1_node_definition.cpp

--- chapter7_qa/q1_node_definition.cpp
+++ chapter7_qa/q1_node_definition.cpp
@@ -1,9 +1,10 @@
 struct node{
-    int data, prev, next;
+    int data, next;
     // so it can be seen that the prev and next is also of
     // integer type rather than pointer
 
     node(int _data){
         data = _data;
+        next = -1;
     }
 };
